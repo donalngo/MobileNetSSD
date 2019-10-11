235ee4fd51929895b165e53ad258baa927f39ca0
vidur6789 <vidur_07@live.com>
2019-10-11 14:07:05 +0800

    change loss function to class as required by Keras

--- models/train_ssd.py
+++ models/train_ssd.py
@@ -1,6 +1,8 @@
 from keras_ssd import build_model
-from keras_ssd import compute_loss
-from keras.optimizers import Adam
+from keras_ssd import SSDLoss
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import losses
+from keras_loss_function.keras_ssd_loss import SSDLoss
 
 model = build_model((300,300,3),
                 10,
@@ -13,6 +15,6 @@ model = build_model((300,300,3),
                 divide_by_stddev=None)
 
 adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-model.compile(optimizer=adam, loss=compute_loss)
-
+ssd_loss = SSDLoss(neg_pos_ratio=3, alpha=1.0)
+model.compile(optimizer=adam, loss=ssd_loss.compute_loss)
 model.summary()
