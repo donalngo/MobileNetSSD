168d1d446774339be4fb710c66c03ad88fbfebe4
Teerapong <ti11abong@hotmail.com>
2019-10-14 17:59:20 +0800

    update

--- models/train_ssd.py
+++ models/train_ssd.py
@@ -44,99 +44,10 @@ model.summary()
 # Directories
 train_image_dir     = '/img_train/'
 
+img_height = 300
+img_width = 300
+n_classes = 3
 
-label_encoder = SSDInputEncoder
-
-train_dataset = datagen.data_generator(img_dir = train_image_dir, xml_dir = train_image_dir, batch_size=10, steps_per_epoch=None, img_sz=300, label_encoder=label_encoder,
-                       translate=0, rotate=0, scale=0, shear=0, hor_flip=True, ver_flip=False)
-#val_dataset = DataGenerator(load_images_into_memory=False, hdf5_dataset_path=None)
-
-# 2: Parse the image and label lists for the training and validation datasets. This can take a while.
-
-# TODO: Set the paths to the datasets here.
-
-# The directories that contain the images.
-VOC_2007_images_dir      = '../../datasets/VOCdevkit/VOC2007/JPEGImages/'
-VOC_2012_images_dir      = '../../datasets/VOCdevkit/VOC2012/JPEGImages/'
-
-# The directories that contain the annotations.
-VOC_2007_annotations_dir      = '../../datasets/VOCdevkit/VOC2007/Annotations/'
-VOC_2012_annotations_dir      = '../../datasets/VOCdevkit/VOC2012/Annotations/'
-
-# The paths to the image sets.
-VOC_2007_train_image_set_filename    = '../../datasets/VOCdevkit/VOC2007/ImageSets/Main/train.txt'
-VOC_2012_train_image_set_filename    = '../../datasets/VOCdevkit/VOC2012/ImageSets/Main/train.txt'
-VOC_2007_val_image_set_filename      = '../../datasets/VOCdevkit/VOC2007/ImageSets/Main/val.txt'
-VOC_2012_val_image_set_filename      = '../../datasets/VOCdevkit/VOC2012/ImageSets/Main/val.txt'
-VOC_2007_trainval_image_set_filename = '../../datasets/VOCdevkit/VOC2007/ImageSets/Main/trainval.txt'
-VOC_2012_trainval_image_set_filename = '../../datasets/VOCdevkit/VOC2012/ImageSets/Main/trainval.txt'
-VOC_2007_test_image_set_filename     = '../../datasets/VOCdevkit/VOC2007/ImageSets/Main/test.txt'
-
-# The XML parser needs to now what object class names to look for and in which order to map them to integers.
-classes = ['background',
-           'aeroplane', 'bicycle', 'bird', 'boat',
-           'bottle', 'bus', 'car', 'cat',
-           'chair', 'cow', 'diningtable', 'dog',
-           'horse', 'motorbike', 'person', 'pottedplant',
-           'sheep', 'sofa', 'train', 'tvmonitor']
-
-train_dataset.parse_xml(images_dirs=[VOC_2007_images_dir,
-                                     VOC_2012_images_dir],
-                        image_set_filenames=[VOC_2007_trainval_image_set_filename,
-                                             VOC_2012_trainval_image_set_filename],
-                        annotations_dirs=[VOC_2007_annotations_dir,
-                                          VOC_2012_annotations_dir],
-                        classes=classes,
-                        include_classes='all',
-                        exclude_truncated=False,
-                        exclude_difficult=False,
-                        ret=False)
-
-val_dataset.parse_xml(images_dirs=[VOC_2007_images_dir],
-                      image_set_filenames=[VOC_2007_test_image_set_filename],
-                      annotations_dirs=[VOC_2007_annotations_dir],
-                      classes=classes,
-                      include_classes='all',
-                      exclude_truncated=False,
-                      exclude_difficult=True,
-                      ret=False)
-
-# Optional: Convert the dataset into an HDF5 dataset. This will require more disk space, but will
-# speed up the training. Doing this is not relevant in case you activated the `load_images_into_memory`
-# option in the constructor, because in that cas the images are in memory already anyway. If you don't
-# want to create HDF5 datasets, comment out the subsequent two function calls.
-
-train_dataset.create_hdf5_dataset(file_path='dataset_pascal_voc_07+12_trainval.h5',
-                                  resize=False,
-                                  variable_image_size=True,
-                                  verbose=True)
-
-val_dataset.create_hdf5_dataset(file_path='dataset_pascal_voc_07_test.h5',
-                                resize=False,
-                                variable_image_size=True,
-                                verbose=True)
-
-#%% Block 4: 
-
-
-# 3: Set the batch size.
-
-batch_size = 32 # Change the batch size if you like, or if you run into GPU memory issues.
-
-# 4: Set the image transformations for pre-processing and data augmentation options.
-
-# For the training generator:
-ssd_data_augmentation = SSDDataAugmentation(img_height=img_height,
-                                            img_width=img_width,
-                                            background=mean_color)
-
-# For the validation generator:
-convert_to_3_channels = ConvertTo3Channels()
-resize = Resize(height=img_height, width=img_width)
-
-# 5: Instantiate an encoder that can encode ground truth labels into the format needed by the SSD loss function.
-
-# The encoder constructor needs the spatial dimensions of the model's predictor layers to create the anchor boxes.
 predictor_sizes = [model.get_layer('classes1').output_shape[1:3],
                    model.get_layer('classes2').output_shape[1:3],
                    model.get_layer('classes3').output_shape[1:3],
@@ -144,49 +55,24 @@ predictor_sizes = [model.get_layer('classes1').output_shape[1:3],
                    model.get_layer('classes5').output_shape[1:3],
                    model.get_layer('classes6').output_shape[1:3]]
 
-ssd_input_encoder = SSDInputEncoder(img_height=img_height,
-                                    img_width=img_width,
-                                    n_classes=n_classes,
-                                    predictor_sizes=predictor_sizes,
-                                    scales=scales,
-                                    aspect_ratios_per_layer=aspect_ratios,
-                                    two_boxes_for_ar1=two_boxes_for_ar1,
-                                    steps=steps,
-                                    offsets=offsets,
-                                    clip_boxes=clip_boxes,
-                                    variances=variances,
-                                    matching_type='multi',
-                                    pos_iou_threshold=0.5,
-                                    neg_iou_limit=0.5,
-                                    normalize_coords=normalize_coords)
 
-# 6: Create the generator handles that will be passed to Keras' `fit_generator()` function.
+label_encoder = SSDInputEncoder(img_height,
+                                img_width,
+                                n_classes,
+                                predictor_sizes,
+                                min_scale=0.1,
+                                max_scale=0.9,
+                                aspect_ratios=[0.5, 1.0, 2.0],
+                                pos_iou_threshold=0.5,
+                                neg_iou_limit=0.3,
+                                border_pixels='half',
+                                normalize_coords=True,
+                                background_id=0)
 
-train_generator = train_dataset.generate(batch_size=batch_size,
-                                         shuffle=True,
-                                         transformations=[ssd_data_augmentation],
-                                         label_encoder=ssd_input_encoder,
-                                         returns={'processed_images',
-                                                  'encoded_labels'},
-                                         keep_images_without_gt=False)
+train_dataset = datagen.data_generator(img_dir = train_image_dir, xml_dir = train_image_dir, batch_size=10, steps_per_epoch=None, img_sz=300, label_encoder=label_encoder,
+                       translate=0, rotate=0, scale=0, shear=0, hor_flip=True, ver_flip=False)
 
-val_generator = val_dataset.generate(batch_size=batch_size,
-                                     shuffle=False,
-                                     transformations=[convert_to_3_channels,
-                                                      resize],
-                                     label_encoder=ssd_input_encoder,
-                                     returns={'processed_images',
-                                              'encoded_labels'},
-                                     keep_images_without_gt=False)
-
-# Get the number of samples in the training and validations datasets.
-train_dataset_size = train_dataset.get_dataset_size()
-val_dataset_size   = val_dataset.get_dataset_size()
-
-print("Number of images in the training dataset:\t{:>6}".format(train_dataset_size))
-print("Number of images in the validation dataset:\t{:>6}".format(val_dataset_size))
-
-#%% Block 5
+#%% Block 4
 
 
 # Define a learning rate schedule.
@@ -225,36 +111,61 @@ callbacks = [model_checkpoint,
              learning_rate_scheduler,
              terminate_on_nan]
 
-#%% Block 6: Training
+#%% Block 5: Training
 
 # If you're resuming a previous training, set `initial_epoch` and `final_epoch` accordingly.
 initial_epoch   = 0
 final_epoch     = 120
 steps_per_epoch = 1000
 
-history = model.fit_generator(generator=train_generator,
+history = model.fit_generator(generator=train_dataset,
                               steps_per_epoch=steps_per_epoch,
                               epochs=final_epoch,
                               callbacks=callbacks,
-                              validation_data=val_generator,
-                              validation_steps=ceil(val_dataset_size/batch_size),
                               initial_epoch=initial_epoch)
 
-#%% Block 7: Predictions
+#history = model.fit_generator(generator=train_dataset,
+#                              steps_per_epoch=steps_per_epoch,
+#                              epochs=final_epoch,
+#                              callbacks=callbacks,
+#                              validation_data=val_generator,
+#                              validation_steps=ceil(val_dataset_size/batch_size),
+#                              initial_epoch=initial_epoch)
 
-predict_generator = val_dataset.generate(batch_size=1,
-                                         shuffle=True,
-                                         transformations=[convert_to_3_channels,
-                                                          resize],
-                                         label_encoder=None,
-                                         returns={'processed_images',
-                                                  'filenames',
-                                                  'inverse_transform',
-                                                  'original_images',
-                                                  'original_labels'},
-                                         keep_images_without_gt=False)
+#%% Block 7a: Predictions on Test Image (With labels)
 
-y_pred = model.predict(batch_images)
+# Directories
+test_image     = 'will get from his generator'
+
+#img_height = 300
+#img_width = 300
+#n_classes = 3
+#
+#train_dataset = datagen.data_generator(img_dir = train_image_dir, xml_dir = train_image_dir, batch_size=10, steps_per_epoch=None, img_sz=300, label_encoder=label_encoder,
+#                       translate=0, rotate=0, scale=0, shear=0, hor_flip=True, ver_flip=False)
+#
+#predict_generator = val_dataset.generate(batch_size=1,
+#                                         shuffle=True,
+#                                         transformations=[convert_to_3_channels,
+#                                                          resize],
+#                                         label_encoder=None,
+#                                         returns={'processed_images',
+#                                                  'filenames',
+#                                                  'inverse_transform',
+#                                                  'original_images',
+#                                                  'original_labels'},
+#                                         keep_images_without_gt=False)
+
+y_pred = model.predict(test_image[0])
+
+#%% Block 7b: Predictions on Test Image (Without labels)
+
+# Directories
+test_image     = '/img_test/'
+
+img_height = 300
+img_width = 300
+n_classes = 3
 
 #%% Block 8: Decoding Predictions
 y_pred_decoded = decode_detections(y_pred,
@@ -265,7 +176,14 @@ y_pred_decoded = decode_detections(y_pred,
                                    img_height=img_height,
                                    img_width=img_width)
 
-y_pred_decoded_inv = apply_inverse_transforms(y_pred_decoded, batch_inverse_transforms)
+#y_pred_decoded_inv = apply_inverse_transforms(y_pred_decoded, batch_inverse_transforms)
+
+        if return_inverter:
+            def inverter(labels):
+                labels = np.copy(labels)
+                labels[:, [ymin+1, ymax+1]] = np.round(labels[:, [ymin+1, ymax+1]] * (img_height / self.out_height), decimals=0)
+                labels[:, [xmin+1, xmax+1]] = np.round(labels[:, [xmin+1, xmax+1]] * (img_width / self.out_width), decimals=0)
+                return labels
 
 np.set_printoptions(precision=2, suppress=True, linewidth=90)
 print("Predicted boxes:\n")
