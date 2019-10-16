9dcbdcf3b4c976a3f068d7c7c068025a81ee4008
Teerapong <ti11abong@hotmail.com>
2019-10-16 16:04:05 +0800

    update

--- models/keras_ssd.py
+++ models/keras_ssd.py
@@ -1839,7 +1839,7 @@ def build_model(image_size,
     n_classes += 1  # Account for the background class.
     l2_reg = l2_regularization  # Make the internal name shorter.
     img_height, img_width, img_channels = image_size[0], image_size[1], image_size[2]
-    clip_boxes = False
+    clip_boxes = True
 
     ############################################################################
     # Get a few exceptions out of the way.
@@ -2037,8 +2037,8 @@ def build_model(image_size,
 
     # Generate the anchor boxes
     # Output shape of `anchors`: `(batch, height, width, n_boxes, 8)`
-    print(conv1.shape)
-    print(boxes1.shape)
+    #print(conv1.shape)
+    #print(boxes1.shape)
     anchors1 = AnchorBoxes(img_height, img_width, this_scale=scales[0], next_scale=scales[1],
                            aspect_ratios=aspect_ratios[0],
                            two_boxes_for_ar1=two_boxes_for_ar1, this_steps=None, this_offsets=None,
@@ -2411,3 +2411,141 @@ class datagen:
         total_img = len(images)
 
         return train_batch_gen, total_img
+
+
+########################################################
+### Decoder
+########################################################
+   
+def _greedy_nms(predictions, iou_threshold=0.45, coords='corners', border_pixels='half'):
+    '''
+    The same greedy non-maximum suppression algorithm as above, but slightly modified for use as an internal
+    function for per-class NMS in `decode_detections()`.
+    '''
+    boxes_left = np.copy(predictions)
+    maxima = [] # This is where we store the boxes that make it through the non-maximum suppression
+    while boxes_left.shape[0] > 0: # While there are still boxes left to compare...
+        maximum_index = np.argmax(boxes_left[:,0]) # ...get the index of the next box with the highest confidence...
+        maximum_box = np.copy(boxes_left[maximum_index]) # ...copy that box and...
+        maxima.append(maximum_box) # ...append it to `maxima` because we'll definitely keep it
+        boxes_left = np.delete(boxes_left, maximum_index, axis=0) # Now remove the maximum box from `boxes_left`
+        if boxes_left.shape[0] == 0: break # If there are no boxes left after this step, break. Otherwise...
+        similarities = iou(boxes_left[:,1:], maximum_box[1:], coords=coords, mode='element-wise', border_pixels=border_pixels) # ...compare (IoU) the other left over boxes to the maximum box...
+        boxes_left = boxes_left[similarities <= iou_threshold] # ...so that we can remove the ones that overlap too much with the maximum box
+    return np.array(maxima)
+
+def decode_detections(y_pred,
+                      confidence_thresh=0.01,
+                      iou_threshold=0.45,
+                      top_k=200,
+                      input_coords='centroids',
+                      normalize_coords=False,
+                      img_height=300,
+                      img_width=300,
+                      border_pixels='half'):
+    '''
+    Convert model prediction output back to a format that contains only the positive box predictions
+    (i.e. the same format that `SSDInputEncoder` takes as input).
+    After the decoding, two stages of prediction filtering are performed for each class individually:
+    First confidence thresholding, then greedy non-maximum suppression. The filtering results for all
+    classes are concatenated and the `top_k` overall highest confidence results constitute the final
+    predictions for a given batch item. This procedure follows the original Caffe implementation.
+    For a slightly different and more efficient alternative to decode raw model output that performs
+    non-maximum suppresion globally instead of per class, see `decode_detections_fast()` below.
+    Arguments:
+        y_pred (array): The prediction output of the SSD model, expected to be a Numpy array
+            of shape `(batch_size, #boxes, #classes + 4 + 4 + 4)`, where `#boxes` is the total number of
+            boxes predicted by the model per image and the last axis contains
+            `[one-hot vector for the classes, 4 predicted coordinate offsets, 4 anchor box coordinates, 4 variances]`.
+        confidence_thresh (float, optional): A float in [0,1), the minimum classification confidence in a specific
+            positive class in order to be considered for the non-maximum suppression stage for the respective class.
+            A lower value will result in a larger part of the selection process being done by the non-maximum suppression
+            stage, while a larger value will result in a larger part of the selection process happening in the confidence
+            thresholding stage.
+        iou_threshold (float, optional): A float in [0,1]. All boxes with a Jaccard similarity of greater than `iou_threshold`
+            with a locally maximal box will be removed from the set of predictions for a given class, where 'maximal' refers
+            to the box score.
+        top_k (int, optional): The number of highest scoring predictions to be kept for each batch item after the
+            non-maximum suppression stage.
+        input_coords (str, optional): The box coordinate format that the model outputs. Can be either 'centroids'
+            for the format `(cx, cy, w, h)` (box center coordinates, width, and height), 'minmax' for the format
+            `(xmin, xmax, ymin, ymax)`, or 'corners' for the format `(xmin, ymin, xmax, ymax)`.
+        normalize_coords (bool, optional): Set to `True` if the model outputs relative coordinates (i.e. coordinates in [0,1])
+            and you wish to transform these relative coordinates back to absolute coordinates. If the model outputs
+            relative coordinates, but you do not want to convert them back to absolute coordinates, set this to `False`.
+            Do not set this to `True` if the model already outputs absolute coordinates, as that would result in incorrect
+            coordinates. Requires `img_height` and `img_width` if set to `True`.
+        img_height (int, optional): The height of the input images. Only needed if `normalize_coords` is `True`.
+        img_width (int, optional): The width of the input images. Only needed if `normalize_coords` is `True`.
+        border_pixels (str, optional): How to treat the border pixels of the bounding boxes.
+            Can be 'include', 'exclude', or 'half'. If 'include', the border pixels belong
+            to the boxes. If 'exclude', the border pixels do not belong to the boxes.
+            If 'half', then one of each of the two horizontal and vertical borders belong
+            to the boxex, but not the other.
+    Returns:
+        A python list of length `batch_size` where each list element represents the predicted boxes
+        for one image and contains a Numpy array of shape `(boxes, 6)` where each row is a box prediction for
+        a non-background class for the respective image in the format `[class_id, confidence, xmin, ymin, xmax, ymax]`.
+    '''
+    #if normalize_coords and ((img_height is None) or (img_width is None)):
+    #    raise ValueError("If relative box coordinates are supposed to be converted to absolute coordinates, the decoder needs the image size in order to decode the predictions, but `img_height == {}` and `img_width == {}`".format(img_height, img_width))
+
+    # 1: Convert the box coordinates from the predicted anchor box offsets to predicted absolute coordinates
+
+    y_pred_decoded_raw = np.copy(y_pred[:,:,:-8]) # Slice out the classes and the four offsets, throw away the anchor coordinates and variances, resulting in a tensor of shape `[batch, n_boxes, n_classes + 4 coordinates]`
+
+    if input_coords == 'centroids':
+        y_pred_decoded_raw[:,:,[-2,-1]] = np.exp(y_pred_decoded_raw[:,:,[-2,-1]] * y_pred[:,:,[-2,-1]]) # exp(ln(w(pred)/w(anchor)) / w_variance * w_variance) == w(pred) / w(anchor), exp(ln(h(pred)/h(anchor)) / h_variance * h_variance) == h(pred) / h(anchor)
+        y_pred_decoded_raw[:,:,[-2,-1]] *= y_pred[:,:,[-6,-5]] # (w(pred) / w(anchor)) * w(anchor) == w(pred), (h(pred) / h(anchor)) * h(anchor) == h(pred)
+        y_pred_decoded_raw[:,:,[-4,-3]] *= y_pred[:,:,[-4,-3]] * y_pred[:,:,[-6,-5]] # (delta_cx(pred) / w(anchor) / cx_variance) * cx_variance * w(anchor) == delta_cx(pred), (delta_cy(pred) / h(anchor) / cy_variance) * cy_variance * h(anchor) == delta_cy(pred)
+        y_pred_decoded_raw[:,:,[-4,-3]] += y_pred[:,:,[-8,-7]] # delta_cx(pred) + cx(anchor) == cx(pred), delta_cy(pred) + cy(anchor) == cy(pred)
+        y_pred_decoded_raw = convert_coordinates(y_pred_decoded_raw, start_index=-4, conversion='centroids2corners')
+    elif input_coords == 'minmax':
+        y_pred_decoded_raw[:,:,-4:] *= y_pred[:,:,-4:] # delta(pred) / size(anchor) / variance * variance == delta(pred) / size(anchor) for all four coordinates, where 'size' refers to w or h, respectively
+        y_pred_decoded_raw[:,:,[-4,-3]] *= np.expand_dims(y_pred[:,:,-7] - y_pred[:,:,-8], axis=-1) # delta_xmin(pred) / w(anchor) * w(anchor) == delta_xmin(pred), delta_xmax(pred) / w(anchor) * w(anchor) == delta_xmax(pred)
+        y_pred_decoded_raw[:,:,[-2,-1]] *= np.expand_dims(y_pred[:,:,-5] - y_pred[:,:,-6], axis=-1) # delta_ymin(pred) / h(anchor) * h(anchor) == delta_ymin(pred), delta_ymax(pred) / h(anchor) * h(anchor) == delta_ymax(pred)
+        y_pred_decoded_raw[:,:,-4:] += y_pred[:,:,-8:-4] # delta(pred) + anchor == pred for all four coordinates
+        y_pred_decoded_raw = convert_coordinates(y_pred_decoded_raw, start_index=-4, conversion='minmax2corners')
+    elif input_coords == 'corners':
+        y_pred_decoded_raw[:,:,-4:] *= y_pred[:,:,-4:] # delta(pred) / size(anchor) / variance * variance == delta(pred) / size(anchor) for all four coordinates, where 'size' refers to w or h, respectively
+        y_pred_decoded_raw[:,:,[-4,-2]] *= np.expand_dims(y_pred[:,:,-6] - y_pred[:,:,-8], axis=-1) # delta_xmin(pred) / w(anchor) * w(anchor) == delta_xmin(pred), delta_xmax(pred) / w(anchor) * w(anchor) == delta_xmax(pred)
+        y_pred_decoded_raw[:,:,[-3,-1]] *= np.expand_dims(y_pred[:,:,-5] - y_pred[:,:,-7], axis=-1) # delta_ymin(pred) / h(anchor) * h(anchor) == delta_ymin(pred), delta_ymax(pred) / h(anchor) * h(anchor) == delta_ymax(pred)
+        y_pred_decoded_raw[:,:,-4:] += y_pred[:,:,-8:-4] # delta(pred) + anchor == pred for all four coordinates
+    else:
+        raise ValueError("Unexpected value for `input_coords`. Supported input coordinate formats are 'minmax', 'corners' and 'centroids'.")
+
+    y_pred_decoded =  y_pred_decoded_raw 
+
+    # 2: If the model predicts normalized box coordinates and they are supposed to be converted back to absolute coordinates, do that
+
+    if normalize_coords:
+        y_pred_decoded_raw[:,:,[-4,-2]] *= img_width # Convert xmin, xmax back to absolute coordinates
+        y_pred_decoded_raw[:,:,[-3,-1]] *= img_height # Convert ymin, ymax back to absolute coordinates
+
+    # 3: Apply confidence thresholding and non-maximum suppression per class
+
+    n_classes = y_pred_decoded_raw.shape[-1] - 4 # The number of classes is the length of the last axis minus the four box coordinates
+
+    y_pred_decoded = [] # Store the final predictions in this list
+    for batch_item in y_pred_decoded_raw: # `batch_item` has shape `[n_boxes, n_classes + 4 coords]`
+        pred = [] # Store the final predictions for this batch item here
+        for class_id in range(1, n_classes): # For each class except the background class (which has class ID 0)...
+            single_class = batch_item[:,[class_id, -4, -3, -2, -1]] # ...keep only the confidences for that class, making this an array of shape `[n_boxes, 5]` and...
+            threshold_met = single_class[single_class[:,0] > confidence_thresh] # ...keep only those boxes with a confidence above the set threshold.
+            if threshold_met.shape[0] > 0: # If any boxes made the threshold...
+                maxima = _greedy_nms(threshold_met, iou_threshold=iou_threshold, coords='corners', border_pixels=border_pixels) # ...perform NMS on them.
+                maxima_output = np.zeros((maxima.shape[0], maxima.shape[1] + 1)) # Expand the last dimension by one element to have room for the class ID. This is now an arrray of shape `[n_boxes, 6]`
+                maxima_output[:,0] = class_id # Write the class ID to the first column...
+                maxima_output[:,1:] = maxima # ...and write the maxima to the other columns...
+                pred.append(maxima_output) # ...and append the maxima for this class to the list of maxima for this batch item.
+        # Once we're through with all classes, keep only the `top_k` maxima with the highest scores
+        if pred: # If there are any predictions left after confidence-thresholding...
+            pred = np.concatenate(pred, axis=0)
+            if top_k != 'all' and pred.shape[0] > top_k: # If we have more than `top_k` results left at this point, otherwise there is nothing to filter,...
+                top_k_indices = np.argpartition(pred[:,1], kth=pred.shape[0]-top_k, axis=0)[pred.shape[0]-top_k:] # ...get the indices of the `top_k` highest-score maxima...
+                pred = pred[top_k_indices] # ...and keep only those entries of `pred`...
+        else:
+            pred = np.array(pred) # Even if empty, `pred` must become a Numpy array.
+        y_pred_decoded.append(pred) # ...and now that we're done, append the array of final predictions for this batch item to the output list
+
+    return y_pred_decoded
--- models/train_ssd v2.py
+++ models/train_ssd v2.py
@@ -4,6 +4,7 @@ import cv2
 from math import ceil
 from keras import backend as K
 from matplotlib import pyplot as plt
+import tensorflow as tf
 
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import losses
@@ -16,8 +17,8 @@ from keras_ssd import datagen
 from keras_ssd import SSDInputEncoder
 from keras_ssd import decode_detections
 from keras_ssd import xml_to_csv
-from keras_ssd import test_generator
 from keras_ssd import read_csv
+from keras_ssd import image_augmentation
 
 #%% Block 1: Building Model
 
@@ -25,12 +26,15 @@ aspect_ratio = [0.5, 1.0, 2.0]
 n_classes = 2
 img_height = 300
 img_width = 300
+img_channels = 3
+min_scale = 0.1
+max_scale = 0.9
 
-model = build_model((300,300,3),
+model = build_model((img_height,img_width,img_channels),
                 n_classes,
                 l2_regularization=0.0,
-                min_scale=0.1,
-                max_scale=0.9,
+                min_scale=min_scale,
+                max_scale=max_scale,
                 aspect_ratios=aspect_ratio,
                 normalize_coords=False,
                 subtract_mean=None,
@@ -48,7 +52,7 @@ model.compile(optimizer=adam, loss=ssd_loss.compute_loss)
 model.summary()
 
 
-#%% Block 3: 
+#%% Block 3: Generator for Training
 
 # Directories
 #train_image_dir     = '/Teerapong/3. Github/1. NUS Masters/CA/MobileNetSSD/models/img_train/'
@@ -66,22 +70,14 @@ predictor_sizes = [model.get_layer('classes1').output_shape[1:3],
                    model.get_layer('classes5').output_shape[1:3],
                    model.get_layer('classes6').output_shape[1:3]]
 
-# aspect_ratios = [[1.0, 2.0, 0.5],
-#                  [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
-#                  [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
-#                  [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
-#                  [1.0, 2.0, 0.5],
-#                  [1.0, 2.0, 0.5]]
-
 
 label_encoder = SSDInputEncoder(img_height,
                                 img_width,
                                 n_classes,
                                 predictor_sizes,
-                                min_scale=0.1,
-                                max_scale=0.9,
+                                min_scale=min_scale,
+                                max_scale=max_scale,
                                 aspect_ratios=aspect_ratio,
-                                #aspect_ratios=aspect_ratios,
                                 pos_iou_threshold=0.5,
                                 neg_iou_limit=0.3,
                                 border_pixels='half',
@@ -151,36 +147,39 @@ history = model.fit_generator(generator=train_dataset,
                               steps_per_epoch=train_steps_per_epoch,
                               epochs=final_epoch)
 
-#history = model.fit_generator(generator=train_dataset,
-#                              steps_per_epoch=steps_per_epoch,
-#                              epochs=final_epoch,
-#                              callbacks=callbacks,
-#                              validation_data=val_generator,
-#                              validation_steps=ceil(val_dataset_size/batch_size),
-#                              initial_epoch=initial_epoch)
 
- #%% Block 7a: Predictions on Test Image (With labels)
+#%% Block 6a: Predictions on Test Image (With labels)
 
-confidence_thresh=0.4,
-iou_threshold=0.1
+confidence_thresh=0.2,
+iou_threshold=0.05
 
 test_image_dir     = 'img_test/'
-
-NumberofTest = len(glob.glob1(test_image_dir,"*.jpg"))
 ImgList = glob.glob("img_test/*.jpg")
-#csv_path = xml_to_csv(test_image_dir)
-#data_csv = read_csv(test_image_dir, "labels.csv")
+
+csv_path = xml_to_csv(test_image_dir)
+data_csv = read_csv(test_image_dir, "labels.csv")
+
+
 y_pred = []
 test_shape =[]
 prediction = []
+total_loss = 0
 
-for i in range(len(ImgList)):   
+for i in range(len(ImgList)):  
+#for i in range(2):  
+    
+    # Getting Ground Truth Y Label
+    image_aug, y_truth = image_augmentation(ImgList[i][9:], img_dir='img_test/', data_csv=data_csv)
+    y_truth = y_truth.reshape(1, y_truth.shape[0], y_truth.shape[1])
+    y_truth = label_encoder(y_truth) 
+      
+    # Getting Predicted Y
     img = cv2.imread(ImgList[i])
     CurrentShape = [img.shape[0], img.shape[1]]
     test_shape.append(CurrentShape)
     
-    img = cv2.resize(img,(300,300))
-    img =img.reshape(1, img.shape[0], img.shape[1],img.shape[2])
+    img = cv2.resize(img,(img_height,img_width))
+    img =img.reshape(1, img.shape[0], img.shape[1], img.shape[2])
     Current_pred = model.predict(img)
     prediction.append(Current_pred)
     y_pred_decoded = decode_detections(Current_pred,
@@ -188,51 +187,38 @@ for i in range(len(ImgList)):
                                     iou_threshold=iou_threshold,
                                     top_k=200)
     y_pred.append(y_pred_decoded)
+    
+    #Computing Loss
+    total_loss = tf.add(ssd_loss.compute_loss(y_truth, Current_pred), total_loss)
 
-y_predcopy = y_pred.copy()
- 
-for i in range(len(y_predcopy)):
-    for j in range(len(y_predcopy[i][0])):
-        # x axis        
-        y_predcopy[i][0][j][2] = y_predcopy[i][0][j][2]/300 * test_shape[i][0]
-        y_predcopy[i][0][j][4] = y_predcopy[i][0][j][4]/300 * test_shape[i][0]
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    x = total_loss.eval()
+    x = x/len(ImgList)
+    print('The total loss for the entire test set is %.2f' % x)
+    
+print("To see the actual img and the bounding box of the test set, please proceed to the next section.")
 
-        # y axis        
-        y_predcopy[i][0][j][3] = y_predcopy[i][0][j][3]/300 * test_shape[i][1]
-        y_predcopy[i][0][j][5] = y_predcopy[i][0][j][5]/300 * test_shape[i][1]
+# Converting Prediction to actual Image Size 
+for i in range(len(y_pred)):
+    for j in range(len(y_pred[i][0])):
+        # x axis with img rows      
+        y_pred[i][0][j][2] = y_pred[i][0][j][2]/img_width * test_shape[i][0]
+        y_pred[i][0][j][4] = y_pred[i][0][j][4]/img_width * test_shape[i][0]
 
-#print(y_pred[0][0][0][5])  
-#print(y_predcopy[0][0][0][5])
-#
-#print(test_shape[0][1])
-#print(len(y_pred))
-#print(np.shape(y_pred[0]))
-#
-#print(y_pred[0])
-#print(y_pred[0][0][0])
+        # y axis with img cols       
+        y_pred[i][0][j][3] = y_pred[i][0][j][3]/img_height * test_shape[i][1]
+        y_pred[i][0][j][5] = y_pred[i][0][j][5]/img_height * test_shape[i][1]
 
-#y_test = test_generator(test_image_dir, data_csv)
-#
-#y_pred = model.predict(y_test)
-
-#test_dataset, test_imgs = datagen.data_generator(img_dir = test_image_dir, xml_dir = test_image_dir, batch_size=batch_size, steps_per_epoch=None, img_sz=300, label_encoder=label_encoder,
-#                       translate=0, rotate=0, scale=1, shear=0, hor_flip=False, ver_flip=False)
-#
-#
-#batch_images = next(test_dataset)
-#
-#y_pred = model.predict(batch_images[0])
-
-#%% Block 9: Plotting Predictions
+#%% Block 7a: Plotting Predictions
 # Set the colors for the bounding boxes
 colors = plt.cm.hsv(np.linspace(0, 1, n_classes+1)).tolist()
 classes = ['background',
             'Fruit', 'Milk']
 
+# Select the test image to display
 CurrentSelect = 0
-print(ImgList[i])
-
-
 
 img = cv2.imread(ImgList[CurrentSelect])
 plt.figure(figsize=(20,12))
@@ -241,15 +227,6 @@ plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 current_axis = plt.gca()
 
 np.shape(y_predcopy[0])
- 
-#for box in batch_original_labels[i]:
-#    xmin = box[1]
-#    ymin = box[2]
-#    xmax = box[3]
-#    ymax = box[4]
-#    label = '{}'.format(classes[int(box[0])])
-#    current_axis.add_patch(plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, color='green', fill=False, linewidth=2))  
-#    current_axis.text(xmin, ymin, label, size='x-large', color='white', bbox={'facecolor':'green', 'alpha':1.0})
 
 for box in y_predcopy[CurrentSelect][0]:
      xmin = box[2]
@@ -261,84 +238,6 @@ for box in y_predcopy[CurrentSelect][0]:
      current_axis.add_patch(plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, color=color, fill=False, linewidth=2))
      current_axis.text(xmin, ymin, label, size='x-large', color='white', bbox={'facecolor':color, 'alpha':1.0})
 
-
-#%%
-     
-imgfile = 'img_test\2.jpg'
-img = cv2.imread(imgfile)
-
-#%%
-np.shape(prediction[0])
-prediction[0][0][0][:-4]
-#%%
-yy=[]
-img = cv2.imread(ImgList[0])
-x = [img.shape[0], img.shape[1]]
-yy.append(x)
-yy.append(x)
-yy[1]
- 
-
-#%%
-#batch_images[1].shape
-y_pred.shape
- #img_height = 300
- #img_width = 300
- #n_classes = 3
- #
- #train_dataset = datagen.data_generator(img_dir = train_image_dir, xml_dir = train_image_dir, batch_size=10, steps_per_epoch=None, img_sz=300, label_encoder=label_encoder,
- #                       translate=0, rotate=0, scale=0, shear=0, hor_flip=True, ver_flip=False)
- #
- #predict_generator = val_dataset.generate(batch_size=1,
- #                                         shuffle=True,
- #                                         transformations=[convert_to_3_channels,
- #                                                          resize],
- #                                         label_encoder=None,
- #                                         returns={'processed_images',
- #                                                  'filenames',
- #                                                  'inverse_transform',
- #                                                  'original_images',
- #                                                  'original_labels'},
- #                                         keep_images_without_gt=False)
-
- #y_pred = model.predict(test_image[0])
-
- #%% Block 7b: Predictions on Test Image (Without labels)
-
- # Directories
- test_image     = '/img_test/'
-
- img_height = 300
- img_width = 300
- n_classes = 3
-
-
-
-#%%
-y_pred[0][3]
-#%% Block 8: Decoding Predictions
-
-y_pred_decoded = decode_detections(y_pred,
-                                    confidence_thresh=0.001,
-                                    iou_threshold=0.001,
-                                    top_k=200)
-
-print("Predicted boxes:\n")
-print('   class   conf xmin   ymin   xmax   ymax')
-print(y_pred_decoded[0])
- #y_pred_decoded_inv = apply_inverse_transforms(y_pred_decoded, batch_inverse_transforms)
-
- #         if return_inverter:
- #             def inverter(labels):
- #                 labels = np.copy(labels)
- #                 labels[:, [ymin+1, ymax+1]] = np.round(labels[:, [ymin+1, ymax+1]] * (img_height / self.out_height), decimals=0)
- #                 labels[:, [xmin+1, xmax+1]] = np.round(labels[:, [xmin+1, xmax+1]] * (img_width / self.out_width), decimals=0)
- #                 return labels
- #
- # np.set_printoptions(precision=2, suppress=True, linewidth=90)
- # print("Predicted boxes:\n")
- # print('   class   conf xmin   ymin   xmax   ymax')
- # print(y_pred_decoded_inv[i])
 
 
 
